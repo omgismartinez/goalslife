cde3ff0570845efeca209ac6bbe6fcac06fa459d
AlvaroMartinezMartinez <72507996+AlvaroMartinezMartinez@users.noreply.github.com>
2021-09-29 04:03:39 -0500

    templating #3

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -22,7 +22,7 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('ng5');
   });
 
-  xit('should render title', () => {
+  it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
